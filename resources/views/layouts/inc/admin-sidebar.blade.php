00b1fe61dce37491f1b1b818f7b1546c5a6f13ea
biju.shrestha <biju.shrestha@info.com.np>
2022-09-15 12:31:19 +0545

    adminSide bar chnage

--- resources/views/layouts/inc/admin-sidebar.blade.php
+++ resources/views/layouts/inc/admin-sidebar.blade.php
@@ -22,7 +22,7 @@
                             <div>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                                 <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                USer
+                                User
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
                             <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
